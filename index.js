d6ddee65107831a92c8fff202b725e75a4f3c89a
Anmol raghav <araghav181@gmail.com>
2024-01-25 23:41:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
